9112a26191a0de0a9ced63a435ccd8399e829b05
Kamalesh S <sankarkamalesh05@gmail.com>
2026-05-24 22:28:32 +0530

    updated env variables

--- app.py
+++ app.py
@@ -36,6 +36,10 @@ if not firebase_admin._apps:
     "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
 }
 
     cred = credentials.Certificate(firebase_config)
