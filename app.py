4ed71f1220db08b7e6c289ba710dbd558d3bac30
Kamalesh S <sankarkamalesh05@gmail.com>
2026-05-25 00:16:46 +0530

    key correction

--- app.py
+++ app.py
@@ -24,28 +24,18 @@ app.secret_key = os.environ.get(
     "SECRET_KEY",
     "change-this-secret-key"
 )
-
 # ---------------- FIREBASE ----------------
-if not firebase_admin._apps:
-    
+import json
 
-    firebase_config = {
-    "type": os.getenv("FIREBASE_TYPE"),
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
-}
+if not firebase_admin._apps:
+
+    with open("serviceAccountKey.json", "r") as f:
+        firebase_config = json.load(f)
 
     cred = credentials.Certificate(firebase_config)
 
     firebase_admin.initialize_app(cred, {
-        "databaseURL": "YOUR_FIREBASE_DATABASE_URL"
+        "databaseURL": firebase_config.get("databaseURL")
     })
 
 # ---------------- QUESTIONS ----------------
